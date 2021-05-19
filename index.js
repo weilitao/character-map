bb3b5a7aff97e84278761a92eeba070d71112c21
weilitao <wlt@live.com>
2021-05-19 12:01:36 +0800

    support some characters have more than one unicode

--- index.js
+++ index.js
@@ -6,33 +6,35 @@
  * for `character-map` command
  */
 
-var opts = require('minimist')(process.argv.slice(2));
-var opentype = require('opentype.js');
-
-if (!opts.f || typeof opts.f !== 'string') {
-	console.log('use -f to specify your font path, TrueType and OpenType supported');
-	return;
-}
-
-opentype.load(opts.f, function(err, font) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-
-	var glyphs = font.glyphs.glyphs;
-	if (!glyphs || glyphs.length === 0) {
-		console.log('no glyphs found in this font');
-		return;
-	}
-
-	var table = '';
-	for (glyphIndex in glyphs) {
-		var glyph = glyphs[glyphIndex];
-		if (glyph.unicode) {
-			table += String.fromCharCode(glyph.unicode);
-		}
-	}
-
-	console.log(table);
-});
+ var opts = require('minimist')(process.argv.slice(2));
+ var opentype = require('opentype.js');
+ 
+ if (!opts.f || typeof opts.f !== 'string') {
+     console.log('use -f to specify your font path, TrueType and OpenType supported');
+     return;
+ }
+ 
+ opentype.load(opts.f, function(err, font) {
+     if (err) {
+         console.log(err);
+         return;
+     }
+ 
+     var glyphs = font.glyphs.glyphs;
+     if (!glyphs || glyphs.length === 0) {
+         console.log('no glyphs found in this font');
+         return;
+     }
+ 
+     var table = [];
+     for (glyphIndex in glyphs) {
+         var glyph = glyphs[glyphIndex];
+         if (glyph.unicode) {
+             glyph.unicodes.forEach(unicode => {
+                table.push(String.fromCharCode(unicode));
+             });
+         }
+     }
+ 
+     console.log(table.join(''));
+ });
